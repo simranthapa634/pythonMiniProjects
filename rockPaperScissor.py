59d9966ee5f12497bcd122f23f677b25f1a31af9
Simran Thapa <simranthapa634@gmail.com>
2022-12-26 11:29:35 -0600

    First comit + Rock Paper Scissor Game

---
+++ rockPaperScissor.py
@@ -0,0 +1,16 @@
+import random
+
+def play():
+    user = input("r for rock: p for paper: s for scissor")
+    computer = random.choice(['r','p','s'])
+    if user == computer:
+        return 'tie'
+    if is_win(user, computer):
+        return "You win"
+    return 'You lost'
+def is_win(player, opponent):
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
+        or (player == 'p' and opponent == 'r'):
+        return True
+    
+print(play())
